46559c04da87dc244771b160d09c035df2af65ab
PaulPPS632 <jeffertsap@hotmail.com>
2025-03-26 21:40:50 -0500

    eliminando carpetas build y dist

--- masivos/build/models/DeudoresLead.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DeudoresLead = void 0;
-const sequelize_typescript_1 = require("sequelize-typescript");
-class DeudoresLead extends sequelize_typescript_1.Model {
-}
-exports.DeudoresLead = DeudoresLead;
